59f5e05b595c3318fe384b655b22667ef5d7d05c
Zeeshan Ahmad <117201162+ZeeshanAhmad85@users.noreply.github.com>
2024-03-21 03:09:51 +0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -43,7 +43,6 @@ def main():
             top_n=1
             top_related_articles = get_top_related_articles(user_input, df, vectorizer, tfidf_matrix,top_n)
             st.write(f"### {top_n} - Top Related Articles")
-            top_related_articles=top_related_articles[['Title','Headings','Author URL','Read Time','Date','Image URL']]
             data_dict = top_related_articles.to_dict(orient='records')
             st.write(data_dict)
             
